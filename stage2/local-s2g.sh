5458bd1b9ea2ed33e5d68dfc306a97d08b2afe34
brunoNetId <bruno.meseguer@gmail.com>
2022-09-05 12:04:47 +0100

    first commit

---
+++ stage2/local-s2g.sh
@@ -0,0 +1,9 @@
+# Define the path to your personal configuration
+CONFIG_PATH=../../chapters_config
+
+camel run \
+flows/s2g.yaml \
+maps/s2g.jslt \
+--local-kamelet-dir=$PWD/../kamelets \
+--properties=$CONFIG_PATH/stage2.properties \
+--property=transform.path=maps/s2g.jslt
