d95687235afc5f681830010eeafb9604a67e96fb
brunoNetId <bruno.meseguer@gmail.com>
2022-09-12 13:10:49 +0100

    docs stage2

--- stage2/local-s2g.sh
+++ stage2/local-s2g.sh
@@ -6,4 +6,5 @@ flows/s2g.yaml \
 maps/s2g.jslt \
 --local-kamelet-dir=$PWD/../kamelets \
 --properties=$CONFIG_PATH/stage2.properties \
---property=transform.path=maps/s2g.jslt
+--property=transform.path=maps/s2g.jslt \
+--dep=net.minidev:json-smart:2.4.8
